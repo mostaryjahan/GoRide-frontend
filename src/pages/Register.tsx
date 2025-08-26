64a39d1d69b6cdd92883f4b84514a1b612c6dce8
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-08-26 21:38:30 +0600

    fix: git push issue

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -4,11 +4,7 @@ const Register = () => {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
+        
       </div>
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex flex-1 items-center justify-center">
